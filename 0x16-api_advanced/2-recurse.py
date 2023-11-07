c9eb06cf3c77c0235fc46f04af052745aff2bd6e
AbdelrahmanMohamed9 <abdelrahmanmohammed049@gmail.com>
2023-11-07 10:33:23 -0800

    all

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,49 +1,35 @@
 #!/usr/bin/python3
-'''A module containing functions for working with the Reddit API.
-'''
+"""Query a list of all hot posts on a given Reddit subreddit"""
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
+def add_title(hot_list, hot_posts):
+    """ Add item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
 
 
-def recurse(subreddit, hot_list=[], n=0, after=None):
-    '''Retrieves a list of hot posts from a given subreddit.
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
+def recurse(subreddit, hot_list=[], after=None):
+    """ Query Reddit API """
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
     }
-    sort = 'hot'
-    limit = 30
-    res = requests.get(
-        '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format(
-            BASE_URL,
-            subreddit,
-            sort,
-            limit,
-            n,
-            after if after else ''
-        ),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        data = res.json()['data']
-        posts = data['children']
-        count = len(posts)
-        hot_list.extend(list(map(lambda x: x['data']['title'], posts)))
-        if count >= limit and data['after']:
-            return recurse(subreddit, hot_list, n + count, data['after'])
-        else:
-            return hot_list if hot_list else None
-    else:
-        return hot_list if hot_list else None
+    params = {
+        'after': after
+    }
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url, headers=headers, params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
+        return None
+
+    body = res.json()
+    hot_posts = body['data']['children']
+    add_title(hot_list, hot_posts)
+    after = body['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
